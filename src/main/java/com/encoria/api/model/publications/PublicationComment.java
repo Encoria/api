f24772283977d1659a6d766ffecbaccf30d45c84
irenegm03 <garciamateosirene@gmail.com>
2025-05-05 15:57:05 +0200

    Create model.publications entities

---
+++ src/main/java/com/encoria/api/model/publications/PublicationComment.java
@@ -0,0 +1,61 @@
+package com.encoria.api.model.publications;
+
+import com.encoria.api.model.ModerationStatus;
+import com.encoria.api.model.users.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "publication_comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PublicationComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publication_id", nullable = false)
+    private Publication publication;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "status_id", nullable = false)
+    private ModerationStatus status;
+
+    @PrePersist
+    public void prePersist() {
+        if (uuid == null) {
+            uuid = UUID.randomUUID();
+        }
+        if (createdAt == null) {
+            createdAt = Instant.now();
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
+}
