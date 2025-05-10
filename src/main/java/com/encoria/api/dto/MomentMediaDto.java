cad7bbc0fe25da4c420f0adcdebd87bbdbab370d
DanielRicM <danielmd2306@gmail.com>
2025-05-10 23:20:15 +0200

    Update MomentMedia (uuid)
    - Remove uuid attribute from MomentMedia entity class and MomentMediaDto

--- src/main/java/com/encoria/api/dto/MomentMediaDto.java
+++ src/main/java/com/encoria/api/dto/MomentMediaDto.java
@@ -2,14 +2,9 @@ package com.encoria.api.dto;
 
 import com.encoria.api.model.moments.MediaType;
 
-import java.util.UUID;
-
 public record MomentMediaDto(
-        UUID uuid,
         String mediaUrl,
         MediaType mediaType,
         Integer position
 ) {
-
-
 }
