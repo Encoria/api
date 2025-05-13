5d7309ce2dd4f4758005d9eede72d521f36035c8
DanielRicM <danielmd2306@gmail.com>
2025-05-13 21:55:34 +0200

    Update MomentMapper
    Implement custom mapping logic for Location to latitude-longitude and vice versa

--- src/main/java/com/encoria/api/mapper/MomentMapper.java
+++ src/main/java/com/encoria/api/mapper/MomentMapper.java
@@ -8,10 +8,15 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = {MomentMediaMapper.class, ArtistMapper.class})
 public interface MomentMapper {
 
-    @Mapping(source = "media", target = "media")
+    @Mapping(source = "location.latitude", target = "latitude")
+    @Mapping(source = "location.longitude", target = "longitude")
     MomentDto toDto(Moment moment);
 
-    @Mapping(source = "media", target = "media")
+    @Mapping(target = "location", expression = "java(toLocation(momentDto.latitude(), momentDto.longitude()))")
     Moment toEntity(MomentDto momentDto);
 
+    default com.encoria.api.model.moments.Location toLocation(Float latitude, Float longitude) {
+        if (latitude == null || longitude == null) return null;
+        return new com.encoria.api.model.moments.Location(latitude, longitude);
+    }
 }
