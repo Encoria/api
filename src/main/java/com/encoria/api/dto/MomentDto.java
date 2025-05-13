95468d439dfadfd54799a8ae5e76e14cbaa446e9
DanielRicM <danielmd2306@gmail.com>
2025-05-13 21:51:32 +0200

    Fix Moment Dtos
    Refactor MomentDto and MomentMediaDto to require simple data types instead of custom objects
    (Location -> Float latitude, Float longitude)
    (MediaType -> String)

--- src/main/java/com/encoria/api/dto/MomentDto.java
+++ src/main/java/com/encoria/api/dto/MomentDto.java
@@ -1,18 +1,16 @@
 package com.encoria.api.dto;
 
-
-import com.encoria.api.model.moments.Location;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-
 public record MomentDto(
         UUID uuid,
         String title,
         String description,
-        Location location,
+        Float latitude,
+        Float longitude,
         LocalDate date,
         List<MomentMediaDto> media,
         Set<ArtistDto> artists
--- src/main/java/com/encoria/api/dto/MomentMediaDto.java
+++ src/main/java/com/encoria/api/dto/MomentMediaDto.java
@@ -1,10 +1,7 @@
 package com.encoria.api.dto;
 
-import com.encoria.api.model.moments.MediaType;
-
 public record MomentMediaDto(
         String mediaUrl,
-        MediaType mediaType,
-        Integer position
-) {
+        String mediaType,
+        Integer position) {
 }
