56bc701ce97b14f46297ec071e73993bda0ba7be
DanielRicM <danielmd2306@gmail.com>
2025-05-15 20:06:34 +0200

    Refactor UserMapper
    - Improve toEntity method by using annotations for jwt claims extraction instead of default implementation

--- src/main/java/com/encoria/api/mapper/UserMapper.java
+++ src/main/java/com/encoria/api/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.encoria.api.mapper;
 
 import com.encoria.api.dto.CreateUserProfileDto;
 import com.encoria.api.dto.UserProfileDto;
-import com.encoria.api.model.users.Country;
 import com.encoria.api.model.users.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,15 +13,7 @@ public interface UserMapper {
     @Mapping(target = "countryCode", expression = "java(user.getCountry() != null ? user.getCountry().getCode() : null)")
     UserProfileDto toDto(User user);
 
-    default User toEntity(CreateUserProfileDto dto, Jwt jwt) {
-        User user = new User();
-        user.setExternalAuthId(jwt.getSubject());
-        user.setEmail(jwt.getClaimAsString("email"));
-        user.setUsername(dto.username());
-        user.setFirstname(dto.firstname());
-        user.setLastname(dto.lastname());
-        user.setBirthdate(dto.birthdate());
-        user.setPictureUrl(dto.pictureUrl());
-        return user;
-    }
+    @Mapping(target = "externalAuthId", expression = "java(jwt.getSubject())")
+    @Mapping(target = "email", expression = "java(jwt.getClaimAsString(\"email\"))")
+    User toEntity(CreateUserProfileDto dto, Jwt jwt);
 }
