5f2c17ce2f8aaab2b9f26e0d343b122d213b3e64
DanielRicM <danielmd2306@gmail.com>
2025-05-25 21:06:38 +0200

    Fix UserSettingsRepository isPrivate method
    - Use JPQL for extracting boolean flag

--- src/main/java/com/encoria/api/repository/UserSettingsRepository.java
+++ src/main/java/com/encoria/api/repository/UserSettingsRepository.java
@@ -2,11 +2,13 @@ package com.encoria.api.repository;
 
 import com.encoria.api.model.users.UserSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserSettingsRepository extends JpaRepository<UserSettings, Long> {
     Optional<UserSettings> findByUserId(Long userId);
 
+    @Query("SELECT u.isPrivateProfile FROM UserSettings u WHERE u.user.id = :userId")
     Boolean isPrivateProfileByUserId(Long userId);
 }
